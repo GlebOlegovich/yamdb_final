9cb128f839b26aa16b20b8c6914a84bc82119afb
GlebOlegovich <godleib@yandex.ru>
2022-02-19 13:40:44 +0300

    уцацацуа

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 from drf_yasg import openapi
+
+
+
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
